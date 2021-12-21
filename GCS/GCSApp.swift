34e500108a8d4c812f4cc0baf5a8e8b9546eea7f
ExpeditionDocs LLC <expeditiondocs@gmail.com>
2021-12-21 18:22:34 -0500

    Initial Commit

---
+++ GCS/GCSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GCSApp.swift
+//  GCS
+//
+//  Created by Joshua Mularella on 12/21/21.
+//
+
+import SwiftUI
+
+@main
+struct GCSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
